fbff08fbf2ce79152ebb31347fe9f20b51efdba8
yanovskaya <yanovskaya1997@list.ru>
2019-01-27 10:45:56 +0300

    first commit

---
+++ PhotoLab/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PhotoLab
+//
+//  Created by Elena Yanovskaya on 27/01/2019.
+//  Copyright © 2019 Elena Yanovskaya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
